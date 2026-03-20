c8b9b5d1c4c5aebdc95d31bae2149fe969232d63
Felix Brown <kimhyunjin6631@gmail.com>
2026-03-21 08:28:13 +0900

    260321-1

--- 2480.c
+++ 2480.c
@@ -5,7 +5,15 @@ int main(void) {
     scanf("%d%d%d",&a,&b,&c);
     if(a==b && b==c)
         printf("%d\n",10000+a*1000);
-    else if(a==b || b==c || c==a) {
-        printf()
+    else if(a==b)
+        printf("%d\n",1000+a*100);
+    else if(a==c)
+        printf("%d\n",1000+a*100);
+    else if(c==b)
+        printf("%d\n",1000+c*100);
+    else {
+        int max = a>b?a:b;
+        printf("%d\n",(max>c?max:c)*100);
     }
+    return 0;
 }
